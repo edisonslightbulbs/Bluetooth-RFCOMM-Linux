50bbda393f0bfa03235e5e41f79e6c155c9100cf
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-06-21 07:22:46 +0200

    implementing header

--- include/server.h
+++ include/server.h
@@ -1,4 +1,17 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#endif //SERVER_H
+/**
+ * setupServer
+ *   Creates a local server and opens a listening channel for a
+ *   specified number of connections.
+ *
+ *   @ref
+ *     https://people.csail.mit.edu/albert/bluez-intro/x502.html
+ *
+ *   accessed
+ *     05:22 18 June 2021
+ */
+void setupSever();
+
+#endif // SERVER_H
