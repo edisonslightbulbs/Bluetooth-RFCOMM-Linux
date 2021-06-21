854a13a0304d447eaa43884cdf9ac8b38781502f
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-06-21 08:28:42 +0200

    code review

--- src/channel.c
+++ src/channel.c
@@ -1,36 +1,30 @@
-// #include <stdio.h>
 #include <stdlib.h>
-// #include <string.h>
-
-// #include <bluetooth/bluetooth.h>
-// #include <bluetooth/sdp.h>
-// #include <bluetooth/sdp_lib.h>
 
 #include "channel.h"
 #include "macros.hpp"
 
 int getChannel(uint8_t* uuid, char deviceAddress[18])
 {
-    /*  connect to sdp server: device must be ON but server need not be running
-     */
+    // connect to an SDP server
     uint8_t address[6];
-    str2ba(deviceAddress, (MAC_ADDRESS*)&address);
+    str2ba(deviceAddress, (ADDRESS*)&address);
     SESSION* session
-        = sdp_connect(BDADDR_ANY, (MAC_ADDRESS*)&address, SDP_RETRY_IF_BUSY);
+        = sdp_connect(BDADDR_ANY, (ADDRESS*)&address, SDP_RETRY_IF_BUSY);
     if (!session) {
         fprintf(stderr, "-- can't connect to sdp server! \n");
         exit(-1);
     }
 
-    /* create query lists */
-    UUID deviceUuid;
-    int range = 0x0000ffff; // from 0000 to ffff
-    sdp_uuid128_create(&deviceUuid, uuid);
+    UUID uuid128;
+    sdp_uuid128_create(&uuid128, uuid);
+
+    // create query lists
+    int range = 0x0000ffff;
     LIST* responseList;
-    LIST* searchList = sdp_list_append(NULL, &deviceUuid);
+    LIST* searchList = sdp_list_append(NULL, &uuid128);
     LIST* attrIdList = sdp_list_append(NULL, &range);
 
-    /* search for records: server must be running */
+    // search for records
     int success = sdp_service_search_attr_req(
         session, searchList, SDP_ATTR_REQ_RANGE, attrIdList, &responseList);
     if (success) {
@@ -38,26 +32,27 @@ int getChannel(uint8_t* uuid, char deviceAddress[18])
         exit(-1);
     }
 
-    /* check responses */
+    // check responses
     success = sdp_list_len(responseList);
     if (success <= 0) {
         fprintf(stderr, "-- no responses! \n");
         exit(-1);
     }
 
-    /* process responses */
+    // process responses
     int channel = 0;
     LIST* responses = responseList;
     while (responses) {
-        LIST *protoList, *protocol;
         RECORD* record = (RECORD*)responses->data;
+
+        LIST* protoList;
         success = sdp_get_access_protos(record, &protoList);
         if (success) {
             fprintf(stderr, "-- can't access protocols! \n");
             exit(-1);
         }
 
-        protocol = protoList;
+        LIST* protocol = protoList;
         while (protocol) {
             LIST* pds;
             int protocolCount = 0;
@@ -96,14 +91,13 @@ int getChannel(uint8_t* uuid, char deviceAddress[18])
         responses = responses->next; // to next response
     }
 
-    // assert channel # no less that 1:
-    // [ in principle, must be in range 1 - 30 ]
+    // assert channel id not less that 1:
+    //   - in principle, a channel id ranges between 1 and 30
     if (channel < 1) {
         fprintf(stderr, "-- no rfcomm channel found using UUID: ");
         fprintf(stderr, "d8308c4e-9469-4051-8adc-7a2663e415e2 \n");
         exit(0);
     }
-
-    printf("-- rfcomm channel: %d\n", channel);
+    printf("-- rfcomm channel id: %d\n", channel);
     return (channel);
 }
