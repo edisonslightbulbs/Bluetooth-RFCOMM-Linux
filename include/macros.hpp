03e5d46c603362675e3d8cdc0f4dbd9ec536633e
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-06-21 08:25:44 +0200

    factoring delay routine into a convinience macro

--- include/macros.hpp
+++ include/macros.hpp
@@ -1,3 +1,5 @@
+// macros to support learnability
+//
 #ifndef MACROS_HPP
 #define MACROS_HPP
 
@@ -6,12 +8,29 @@
 #include <bluetooth/sdp.h>
 #include <bluetooth/sdp_lib.h>
 #include <sys/socket.h>
+#include <time.h>
 
 #define UUID uuid_t
+#define ADDRESS bdaddr_t
+
 #define LIST sdp_list_t
 #define DATA sdp_data_t
 #define RECORD sdp_record_t
-#define MAC_ADDRESS bdaddr_t
 #define SESSION sdp_session_t
+
+#define RX_TX_DELAY 1
+
+#if RX_TX_DELAY == 1
+#define SECONDS 1
+#define BEGIN_DELAY_ENVIRONMENT while (true) {
+#define END_DELAY_ENVIRONMENT                                                  \
+    unsigned int retTime = time(0) + SECONDS;                                  \
+    while (time(0) < retTime)                                                  \
+        ;                                                                      \
+    }
+#else
+#define START_DELAY
+#define STOP_DELAY
+#endif
 
 #endif // MACROS_HPP
